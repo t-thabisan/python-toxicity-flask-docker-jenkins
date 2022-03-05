ed73f08f8e2b5c8daee0d4047e8ba58c38298df1
tang.hugo1999 <tang.hugo1999@gmail.com>
2022-03-05 21:51:36 +0100

    Detach model from function

--- backend/app/model/detoxifyModel.py
+++ backend/app/model/detoxifyModel.py
@@ -2,8 +2,10 @@ from flask import jsonify
 from detoxify import Detoxify
 import pandas as pd
 
+toxicity_model = Detoxify('original')
+
 def toxicity_check(sentence): 
-    results = Detoxify('original').predict(sentence)
+    results = toxicity_model.predict(sentence)
     df = pd.DataFrame(results, index=["value"]).round(2)
     json = df.to_json()
     return json

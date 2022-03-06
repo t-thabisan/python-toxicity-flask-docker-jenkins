18f6b06eccac5ff9aa5868e1c033950ae4802afb
tang.hugo1999 <tang.hugo1999@gmail.com>
2022-03-06 17:13:12 +0100

    Adding feature prometheus / node exporter / prom python client+server

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for
 from routes.toxicity_bp import toxicity_bp
+from prometheus_client import start_http_server
 
 app = Flask(__name__)
 app.register_blueprint(toxicity_bp, url_prefix='/api')
@@ -10,3 +11,4 @@ def index():
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+start_http_server(8010)
